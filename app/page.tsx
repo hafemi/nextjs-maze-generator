fbbc7d7b55f727a1368a4ec021f640f4c731084a
Hafemi <118757157+hafemi@users.noreply.github.com>
2024-09-15 13:28:59 +0200

    feat: new fields and DRY functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
-import styles from "./page.module.css";
 import {
   handleGenerationButtonClicked,
   handleSolutionButtonClicked,
 } from "./maze-button-handler";
+import {
+  inputBuilder,
+  dropdownBuilder,
+  buttonBuilder
+} from "./page-utils";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -11,40 +16,15 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.heading}>MAZE GENERATOR</h1>
         <div className={styles.inputContainer}>
-          <div className={styles.inputGroup}>
-            <label htmlFor="width">Width</label>
-            <input
-              type="number"
-              id="width"
-              name="width"
-              placeholder="20"
-            ></input>
-          </div>
-          <div className={styles.inputGroup}>
-            <label htmlFor="height">Height</label>
-            <input
-              type="number"
-              id="height"
-              name="height"
-              placeholder="20"
-            ></input>
-          </div>
+          {inputBuilder("width", "Width", "number", "1-100")}
+          {inputBuilder("height", "Height", "number", "1-100")}
+          {inputBuilder("inner width", "Inner Width", "number", "1-5")}
+          {inputBuilder("inner height", "Inner Height", "number", "1-5")}
+          {dropdownBuilder("startingpoint", "Starts at:", ["Top", "Bottom", "Left", "Right"])}
         </div>
         <div>
-          <button
-            className={styles.button}
-            id="generate"
-            onClick={handleGenerationButtonClicked}
-          >
-            ⚙️ Generate
-          </button>
-          <button
-            className={styles.button}
-            id="solution"
-            onClick={handleSolutionButtonClicked}
-          >
-            🚩 Solution
-          </button>
+          {buttonBuilder("generate", "⚙️ Generate", handleGenerationButtonClicked)}
+          {buttonBuilder("solution", "🚩 Solution", handleSolutionButtonClicked)}
         </div>
       </main>
     </div>
