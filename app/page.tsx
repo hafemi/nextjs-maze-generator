c0ffec300ebfe20f7439b8e63afb06c8e07368a5
Hafemi <118757157+hafemi@users.noreply.github.com>
2024-09-15 15:48:10 +0200

    read desc
    removed useless styling (gonna do it at the end)
    new way to handle inputfields

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,123 @@
 "use client";
+import { useState } from "react";
+import { FaFlag, FaGear } from "react-icons/fa6";
 import {
   handleGenerationButtonClicked,
   handleSolutionButtonClicked,
 } from "./maze-button-handler";
-import {
-  inputBuilder,
-  dropdownBuilder,
-  buttonBuilder
-} from "./page-utils";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [width, setWidth] = useState("");
+  const [height, setHeight] = useState("");
+  const [innerWidth, setInnerWidth] = useState("");
+  const [innerHeight, setInnerHeight] = useState("");
+  const [startingPoint, setStartingPoint] = useState("top");
+  const [invalidElements, setInvalidElements] = useState<string[]>([]);
+
+  const validateElement = (
+    value: number,
+    min: number,
+    max: number,
+    elementId: string
+  ): void => {
+    if (!value || value < min || value > max) {
+      setInvalidElements([...invalidElements, elementId]);
+      return;
+    }
+
+    setInvalidElements(invalidElements.filter((id) => id !== elementId));
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 className={styles.heading}>MAZE GENERATOR</h1>
-        <div className={styles.inputContainer}>
-          {inputBuilder("width", "Width", "number", "1-100")}
-          {inputBuilder("height", "Height", "number", "1-100")}
-          {inputBuilder("inner width", "Inner Width", "number", "1-5")}
-          {inputBuilder("inner height", "Inner Height", "number", "1-5")}
-          {dropdownBuilder("startingpoint", "Starts at:", ["Top", "Bottom", "Left", "Right"])}
+        <div>
+          <label htmlFor="width">Width</label>
+          <input
+            className={`
+              ${invalidElements.includes("width") ? styles.invalid : ""}
+              ${styles.input}
+            `}
+            id="width"
+            type="number"
+            placeholder="1-100"
+            value={width}
+            max={30}
+            onChange={(e) => {
+              setWidth(e.target.value);
+              validateElement(parseInt(e.target.value), 1, 100, "width");
+            }}
+          />
+          <br />
+          <label htmlFor="height">Height</label>
+          <input
+            className={`
+              ${invalidElements.includes("height") ? styles.invalid : ""}
+              ${styles.input}
+            `}
+            id="height"
+            type="number"
+            placeholder="1-100"
+            value={height}
+            onChange={(e) => {
+              setHeight(e.target.value);
+              validateElement(parseInt(e.target.value), 1, 100, "height");
+            }}
+          />
+          <br />
+          <label htmlFor="innerWidth">Inner Width</label>
+          <input
+            className={`
+              ${invalidElements.includes("innerWidth") ? styles.invalid : ""}
+              ${styles.input}
+            `}
+            id="innerWidth"
+            type="number"
+            placeholder="1-5"
+            value={innerWidth}
+            onChange={(e) => {
+              setInnerWidth(e.target.value);
+              validateElement(parseInt(e.target.value), 1, 5, "innerWidth");
+            }}
+          />
+          <br />
+          <label htmlFor="innerHeight">Inner Height</label>
+          <input
+            className={`
+              ${invalidElements.includes("innerHeight") ? styles.invalid : ""}
+              ${styles.input}
+            `}
+            id="innerHeight"
+            type="number"
+            placeholder="1-5"
+            value={innerHeight}
+            onChange={(e) => {
+              setInnerHeight(e.target.value);
+              validateElement(parseInt(e.target.value), 1, 5, "innerHeight");
+            }}
+          />
+          <br />
+          <label htmlFor="startingPoint">Starting Point</label>
+          <select
+            id="startingPoint"
+            value={startingPoint}
+            onChange={(e) => setStartingPoint(e.target.value)}
+          >
+            <option value="top">Top</option>
+            <option value="bottom">Bottom</option>
+            <option value="left">Left</option>
+            <option value="right">Right</option>
+          </select>
         </div>
         <div>
-          {buttonBuilder("generate", "⚙️ Generate", handleGenerationButtonClicked)}
-          {buttonBuilder("solution", "🚩 Solution", handleSolutionButtonClicked)}
+          <button onClick={handleGenerationButtonClicked}>
+            <FaGear /> Generate
+          </button>
+          <button onClick={handleSolutionButtonClicked}>
+            <FaFlag /> Solution
+          </button>
         </div>
       </main>
     </div>
